eb08eec17bdf2c943bae797367b5a027273bc56d
Julianna Messineo <160360849+mathematiCode@users.noreply.github.com>
2025-01-03 12:21:45 -0600

    form partially works

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,91 @@ import CompletedResults from './components/CompletedResults';
 import IncompleteResults from './components/IncompleteResults';
 
 function App() {
+  const [status, setStatus] = useState('empty');
+  const [amount, setAmount] = useState('');
+  const [term, setTerm] = useState('');
+  const [interestRate, setInterestRate] = useState('');
+  const [mortgageType, setMortgageType] = useState('none');
   const [monthlyRepayment, setMonthlyRepayment] = useState(0);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('amount is', amount);
+    console.log('term is', term);
+    if (amount > 0 && term > 0) {
+      setMonthlyRepayment(amount / term / 12);
+    }
+    if (amount > 0 && term > 0 && interestRate >= 0 && mortgageType) {
+      setStatus('success');
+      if (mortgageType == 'repayment') {
+        setMonthlyRepayment(1000);
+      } else if (mortgageType == 'interest-only') {
+        setMonthlyRepayment(200);
+      } else setMonthlyRepayment(5);
+    } else if (amount < 0 || term < 0 || interestRate < 0 || mortgageType) {
+      setStatus('inProgress');
+    }
+  }
 
   return (
     <div className="main">
-      <form>
+      <form onSubmit={event => handleSubmit(event)}>
         <div className="title-and-button">
           <h1>Mortgage Calculator</h1>
           <button className="clear-button">Clear All</button>
         </div>
         <label htmlFor="mortgageAmount" className="label-on-top">
           Mortgage Amount
-          <input type="number" id="mortgageAmount" />
+          <input
+            type="number"
+            id="mortgageAmount"
+            value={amount}
+            onChange={event => setAmount(event.target.value)}
+          />
         </label>
         <div className="flex-horizontal">
           <label htmlFor="mortgageTerm" className="label-on-top">
             Mortgage Term
-            <input type="number" id="mortgageTerm" />
+            <input
+              type="number"
+              id="mortgageTerm"
+              value={term}
+              onChange={event => setTerm(event.target.value)}
+            />
           </label>
           <label htmlFor="interestRate" className="label-on-top">
             Interest Rate
-            <input type="number" id="interestRate" />
+            <input
+              type="number"
+              id="interestRate"
+              value={interestRate}
+              onChange={event => setInterestRate(event.target.value)}
+            />
           </label>
         </div>
 
         <fieldset>
           <legend>Mortgage Type </legend>
-          <label htmlFor="repayment">
-            <input type="radio" name="mortgageType" id="repayment" />
+          <label htmlFor="repayment" className="mortgage-type">
+            <input
+              type="radio"
+              name="mortgageType"
+              id="repayment"
+              value={'repayment'}
+              checked={mortgageType === 'repayment'}
+              onChange={event => setMortgageType(event.target.value)}
+            />
             Repayment
           </label>
-          <label htmlFor="interest-only" className="selected-mortgage-type">
-            <input type="radio" name="mortgageType" id="interest-only" />
+          <label htmlFor="interest-only" className="mortgage-type">
+            <input
+              type="radio"
+              name="mortgageType"
+              id="interest-only"
+              value="interest-only"
+              checked={mortgageType === 'interest-only'}
+              onChange={event => setMortgageType(event.target.value)}
+            />
             Interest Only
           </label>
         </fieldset>
@@ -45,8 +98,11 @@ function App() {
         </button>
       </form>
       <div className="output-container">
-        <IncompleteResults />
-        {/* <CompletedResults monthlyRepayment={monthlyRepayment} /> */}
+        {status == 'success' ? (
+          <CompletedResults monthlyRepayment={monthlyRepayment} />
+        ) : (
+          <IncompleteResults />
+        )}
       </div>
     </div>
   );
--- src/components/IncompleteResults.jsx
+++ src/components/IncompleteResults.jsx
@@ -1,13 +1,13 @@
 function IncompleteResults() {
   return (
-    <>
+    <div className="incomplete-results">
       <img src="./assets/images/illustration-empty.svg" />
       <h2> Results shown here</h2>
       <p>
         Complete the form and click “calculate repayments” to see what your
         monthly repayments would be.
       </p>
-    </>
+    </div>
   );
 }
 

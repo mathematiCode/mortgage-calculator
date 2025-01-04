540e5b3b3b3391ff73960cb860862d509c084b91
Julianna Messineo <160360849+mathematiCode@users.noreply.github.com>
2025-01-04 17:20:52 -0600

    added units to all inputs

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { mortgageCalculator } from '@jdizm/finance-calculator';
 function App() {
   const [status, setStatus] = useState('empty');
   const [amount, setAmount] = useState('');
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('none');
   const [interestRate, setInterestRate] = useState('');
   const [mortgageType, setMortgageType] = useState('none');
   const [monthlyRepayment, setMonthlyRepayment] = useState(0);
@@ -49,16 +49,23 @@ function App() {
     } else if (amount < 0 || term < 0 || interestRate < 0) {
       setStatus('error');
     } else if (
+      amount == undefined &&
+      term == 'none' &&
+      interestRate == undefined &&
+      mortgageType == 'none'
+    ) {
+      setStatus('empty');
+    } else if (
       amount == undefined ||
-      term == undefined ||
+      term == 'none' ||
       interestRate == undefined ||
       mortgageType == 'none'
     ) {
-      console.log({ amount, term, interestRate, mortgageType });
-      setStatus('empty');
+      setStatus('inprogress');
     } else {
       throw new Error('This should never happen');
     }
+    console.log('status', status);
   }
 
   function clearAll() {
@@ -80,39 +87,51 @@ function App() {
         </div>
         <label htmlFor="mortgageAmount" className="label-on-top">
           Mortgage Amount
-          <input
-            type="number"
-            id="mortgageAmount"
-            value={amount}
-            onChange={event => setAmount(event.target.value)}
-          />
+          <div className="amount-container">
+            <div className="front-unit">$</div>
+            <input
+              type="number"
+              id="mortgageAmount"
+              value={amount}
+              onChange={event => setAmount(event.target.value)}
+            />
+          </div>
         </label>
+
         <div className="flex-horizontal">
-          <label htmlFor="mortgageTerm" className="label-on-top">
+          <label htmlFor="mortgage-term" className="label-on-top">
             Mortgage Term
-            <select
-              id="mortgage-term"
-              value={term}
-              onChange={event => setTerm(event.target.value)}
-            >
-              <option value="none">Select Term</option>
-              <optgroup label="term-options">
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
-                <option value="30">30</option>
-              </optgroup>
-            </select>
+            <div className="term-container">
+              <select
+                id="mortgage-term"
+                value={term}
+                onChange={event => setTerm(event.target.value)}
+              >
+                <option value="none">Select Term</option>
+                <optgroup label="term-options">
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                  <option value="30">30</option>
+                </optgroup>
+              </select>
+              <div id="years-unit" className="back-unit">
+                years
+              </div>
+            </div>
           </label>
           <label htmlFor="interestRate" className="label-on-top">
             Interest Rate
-            <input
-              type="number"
-              id="interestRate"
-              value={interestRate}
-              onChange={event => setInterestRate(event.target.value)}
-            />
+            <div className="interest-container">
+              <input
+                type="number"
+                id="interestRate"
+                value={interestRate}
+                onChange={event => setInterestRate(event.target.value)}
+              />
+              <div className="back-unit">%</div>
+            </div>
           </label>
         </div>
 
